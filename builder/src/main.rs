ad65764f4b867da64e56849723fb3ef6d28b07ea
kokeshiM0chi <frogandfrog009@gmail.com>
2023-10-07 22:04:28 +0900

    tmp

---
+++ builder/src/main.rs
@@ -0,0 +1,21 @@
+
+use derive_builder::Builder;
+
+#[derive(Builder)]
+pub struct Command {
+    executable: String,
+    args: Vec<String>,
+    env: Vec<String>,
+    current_dir: String,
+}
+
+fn main() {
+	println!("hello");
+	let mut builder = Command::builder();
+    builder.executable("cargo".to_owned());
+    builder.args(vec!["build".to_owned(), "--release".to_owned()]);
+    builder.env(vec![]);
+    builder.current_dir("..".to_owned());
+
+	
+}
